1e34ec41386e956647fd6a68c464f5e08e4f444e
o0Ignition0o <jeremy.lempereur@gmail.com>
2022-05-18 15:13:59 +0200

    initial poke at syn

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,26 @@ use proc_macro::TokenStream;
 use quote::quote;
 use std::collections::HashMap;
 use syn::{
-    parse::{Parse, ParseStream, Parser, Result},
-    punctuated::Punctuated,
-    Expr, ExprAssign, LitStr, Token,
+    parse::{self, Parse, ParseStream, Parser, Peek, Result},
+    parse_macro_input,
+    punctuated::{self, Punctuated},
+    token::Comma,
+    DeriveInput, Expr, ExprAssign, Ident, LitStr, Token,
 };
 
 #[proc_macro]
 pub fn block(input: TokenStream) -> TokenStream {
-    let ParsedQuery { datalog, .. } = syn::parse(input).unwrap();
+    // litteral / separator
+    // punctuated (comma separated)
+    // ident = expression
+
+    let ParsedQuery {
+        datalog,
+        parameters,
+    } = syn::parse(input).unwrap();
+
+    dbg!(&datalog);
+    dbg!(&parameters);
 
     let mut builder = BlockBuilder::new();
     builder.add_code(&datalog).unwrap();
@@ -19,6 +31,7 @@ pub fn block(input: TokenStream) -> TokenStream {
     let gen = quote! {
       #datalog
     };
+
     gen.into()
 }
 
@@ -29,22 +42,53 @@ struct ParsedQuery {
 
 impl Parse for ParsedQuery {
     fn parse(input: ParseStream) -> Result<Self> {
-        // we expect a string literal here, we let syn extract it
-        let query: LitStr = input.parse()?;
-        let string = query.value();
+        let datalog = input.parse::<LitStr>()?.value();
 
         let mut parameters = HashMap::new();
 
-        let _: Token![,] = input.parse()?;
-        let pairs: ExprAssign = input.parse()?;
+        while input.peek(Token![,]) {
+            let _: Token![,] = input.parse()?;
+            if input.is_empty() {
+                break;
+            }
 
-        if !input.is_empty() {
-            return Err(syn::parse::Error::new(input.span(), "unpexected token"));
+            let key: Ident = input.parse()?;
+            let _: Token![=] = input.parse()?;
+            let value: Expr = input.parse()?;
+
+            parameters.insert(key.to_string(), value);
         }
 
         Ok(ParsedQuery {
-            datalog: string,
+            datalog,
             parameters,
         })
     }
 }
+
+// struct ParsedQuery {
+//     datalog: String,
+//     parameters: HashMap<String, Expr>,
+// }
+
+// impl Parse for ParsedQuery {
+//     fn parse(input: ParseStream) -> Result<Self> {
+//         // we expect a string literal here, we let syn extract it
+//         let query: LitStr = input.parse()?;
+//         let string = query.value();
+
+//         let mut parameters = HashMap::new();
+
+//         let _: Token![,] = input.parse()?;
+//         let pairs: ExprAssign = input.parse()?;
+
+//         if !input.is_empty() {
+//             return Err(syn::parse::Error::new(input.span(), "unpexected token"));
+//         }
+
+//         Ok(ParsedQuery {
+//             datalog: string,
+//             parameters,
+//         })
+//     }
+// }
